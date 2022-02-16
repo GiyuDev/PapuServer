f06d55ef2d5f51843ef3052fbcc391ce1adb90cc
Paulo Andre Monterroso Navas <84726204+GiyuDev@users.noreply.github.com>
2022-02-16 13:50:04 -0600

    a

--- PapuBackpacks/src/com/bitnet/paulo/papubackpacks/Main.java
+++ PapuBackpacks/src/com/bitnet/paulo/papubackpacks/Main.java
@@ -2,10 +2,23 @@ package com.bitnet.paulo.papubackpacks;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.bitnet.paulo.papubackpacks.database.Database;
+import com.bitnet.paulo.papubackpacks.manager.BackpackManager;
+
 public class Main extends JavaPlugin {
+	
+	private BackpackManager manager;
+	
+	private Database database;
 	
 	@Override
 	public void onEnable() {
 		this.getLogger().info("Cargando el plugin...");
+		this.database = new Database(this);
+		this.manager = new BackpackManager(this, database);
+	}
+
+	public BackpackManager getManager() {
+		return manager;
 	}
 }
---
+++ PapuBackpacks/src/com/bitnet/paulo/papubackpacks/listeners/PlayerListeners.java
@@ -0,0 +1,7 @@
+package com.bitnet.paulo.papubackpacks.listeners;
+
+import org.bukkit.event.Listener;
+
+public class PlayerListeners implements Listener {
+
+}
